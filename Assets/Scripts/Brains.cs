8203df7c4d3bbc7b8330eca5e63a8fb70ee88bbb
DeimisM <117855291+DeimisM@users.noreply.github.com>
2024-05-09 19:14:50 +0300

    path

--- Assets/Scripts/Brains.cs
+++ Assets/Scripts/Brains.cs
@@ -3,16 +3,31 @@ using UnityEngine;
 public class Brains : MonoBehaviour
 {
     Vehicle vehicle;
+    Path path;
+    Vector3 targetPos;
 
     void Start()
     {
         vehicle = GetComponent<Vehicle>();
+        path = FindObjectOfType<Path>();
+
+        transform.position = path.GetClosestPoint(transform.position);
+        targetPos = path.GetNextPoint(transform.position);
     }
 
     void Update()
     {
-        print(Mathf.PerlinNoise1D(Time.time));
-        vehicle.Steer(Mathf.PerlinNoise1D(Time.time) * 2 - 1);
+        Debug.DrawLine(transform.position, targetPos, Color.red);
+
+        if(Vector3.Distance(transform.position, targetPos) < 3)
+        {
+            targetPos = path.GetNextPoint(transform.position);
+        }
+
+        var angle = Vector3.SignedAngle(transform.forward, targetPos - transform.position, Vector3.up);
+
+        vehicle.Steer(angle);
+        //vehicle.Steer(Mathf.PerlinNoise1D(Time.time) * 2 - 1);
         vehicle.Accelerate();
     }
 }
